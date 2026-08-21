c0d86fb06c219845a5135623f36a0490ec819663
johnmaged07 <johnmaged07@gmail.com>
2026-08-21 03:54:52 +0300

    Solved python code question
    Refactored monitor_following_distance function to track tailgating incidents and calculate minimum distance.

--- Coding_Assignment/Question.py
+++ Coding_Assignment/Question.py
@@ -1,15 +1,21 @@
 def monitor_following_distance(distances: list[float], speeds: list[float]) -> tuple[int, float, int]:
-    """
-    Analyzes following distance compared to safe distance (speed * 0.5).
-    
-    Args:
-        distances (list[float]): Distance to the lead car at each second.
-        speeds (list[float]): Speed of our car at each second.
+    Tailgating=False
+    tailgating_seconds=0
+    tailgate_incidents=0
+    for i in range(len(distances)):
+        if distances[i] < speeds[i]*0.5:
+            tailgating_seconds+=1
+            if Tailgating==False:
+                Tailgating=True
+                tailgate_incidents+=1
+        else:
+            Tailgating=False
+
+    if distances != []:
+        minimum_distance=min(distances)
+    else:
+        minimum_distance=0.0
         
-    Returns:
-        tuple[int, float, int]: (tailgating_seconds, minimum_distance, tailgate_incidents)
-            - tailgating_seconds: total seconds distance was < safe distance
-            - minimum_distance: absolute closest distance to the lead car (return 0.0 if empty list)
-            - tailgate_incidents: number of separate instances the car started tailgating
-    """
+    return (tailgating_seconds,minimum_distance,tailgate_incidents) 
+
     pass
